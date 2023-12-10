e7859b5dea8cffd1379b319e8d091f3a0a4668c7
David Fokkema <davidfokkema@icloud.com>
2023-12-10 13:27:11 +0100

    Basic script to reset printer name

---
+++ src/fix_canon_name/basic_script.py
@@ -0,0 +1,26 @@
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+PASSWORD = "1234"
+
+RE_NAME = "(?P<name>.*?) \([0-9a-f:]{8}\)"
+
+
+options = webdriver.ChromeOptions()
+options.accept_insecure_certs = True
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://canoncfcad9.local./login.html")
+driver.find_element(By.ID, "i0012A").click()
+driver.find_element(By.ID, "i2101").send_keys(PASSWORD)
+driver.find_element(By.ID, "submitButton").click()
+
+driver.get("https://canoncfcad9.local./m_network_airprint_edit.html")
+name_element = driver.find_element(By.ID, "i2072")
+current_name = name_element.get_attribute("value")
+printer_name = re.match(RE_NAME, current_name).group("name")
+name_element.clear()
+name_element.send_keys(printer_name)
+driver.find_element(By.ID, "submitButton").click()
