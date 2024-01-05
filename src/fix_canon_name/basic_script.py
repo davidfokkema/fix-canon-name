bbf1a49b322dd49011cca73639d79ca9c39eb6fe
David Fokkema <davidfokkema@icloud.com>
2024-01-05 17:06:42 +0100

    Use Firefox and a context manager
    Firefox, just because

--- src/fix_canon_name/basic_script.py
+++ src/fix_canon_name/basic_script.py
@@ -7,20 +7,18 @@ PASSWORD = "1234"
 
 RE_NAME = "(?P<name>.*?) \([0-9a-f:]{8}\)"
 
-
-options = webdriver.ChromeOptions()
+options = webdriver.FirefoxOptions()
 options.accept_insecure_certs = True
-driver = webdriver.Chrome(options=options)
+with webdriver.Firefox(options=options) as driver:
+    driver.get("https://canoncfcad9.local./login.html")
+    driver.find_element(By.ID, "i0012A").click()
+    driver.find_element(By.ID, "i2101").send_keys(PASSWORD)
+    driver.find_element(By.ID, "submitButton").click()
 
-driver.get("https://canoncfcad9.local./login.html")
-driver.find_element(By.ID, "i0012A").click()
-driver.find_element(By.ID, "i2101").send_keys(PASSWORD)
-driver.find_element(By.ID, "submitButton").click()
-
-driver.get("https://canoncfcad9.local./m_network_airprint_edit.html")
-name_element = driver.find_element(By.ID, "i2072")
-current_name = name_element.get_attribute("value")
-printer_name = re.match(RE_NAME, current_name).group("name")
-name_element.clear()
-name_element.send_keys(printer_name)
-driver.find_element(By.ID, "submitButton").click()
+    driver.get("https://canoncfcad9.local./m_network_airprint_edit.html")
+    name_element = driver.find_element(By.ID, "i2072")
+    current_name = name_element.get_attribute("value")
+    printer_name = re.match(RE_NAME, current_name).group("name")
+    name_element.clear()
+    name_element.send_keys(printer_name)
+    driver.find_element(By.ID, "submitButton").click()
