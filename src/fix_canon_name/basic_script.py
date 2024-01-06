8b06d04c8e807e4b4ea2e8524c3c2a898040a69d
David Fokkema <davidfokkema@icloud.com>
2024-01-06 16:37:01 +0100

    Don't fix name if it does not contain partial MAC address

--- src/fix_canon_name/basic_script.py
+++ src/fix_canon_name/basic_script.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 PASSWORD = "1234"
 
-RE_NAME = "(?P<name>.*?) \([0-9a-f:]{8}\)"
+RE_NAME = "(?P<name>.*) \([0-9a-f:]{8}\)"
 
 options = webdriver.FirefoxOptions()
 # options.add_argument("--headless")
@@ -19,7 +19,8 @@ with webdriver.Firefox(options=options) as driver:
     driver.get("https://canoncfcad9.local./m_network_airprint_edit.html")
     name_element = driver.find_element(By.ID, "i2072")
     current_name = name_element.get_attribute("value")
-    printer_name = re.match(RE_NAME, current_name).group("name")
-    name_element.clear()
-    name_element.send_keys(printer_name)
-    driver.find_element(By.ID, "submitButton").click()
+    if match := re.match(RE_NAME, current_name):
+        printer_name = match.group("name")
+        name_element.clear()
+        name_element.send_keys(printer_name)
+        driver.find_element(By.ID, "submitButton").click()
