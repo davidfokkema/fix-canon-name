0d5668a12be139f69d8375cbe23e8d78136b7009
David Fokkema <davidfokkema@icloud.com>
2024-02-11 12:22:37 +0100

    WIP: resetting printer and removing from list

--- src/fix_canon_name/tui.py
+++ src/fix_canon_name/tui.py
@@ -1,6 +1,10 @@
 import asyncio
+import hashlib
+import re
 
 import rich
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from textual import on, work
 from textual.app import App, ComposeResult
 from textual.binding import Binding
@@ -11,6 +15,7 @@ from zeroconf import ServiceStateChange, Zeroconf
 from zeroconf.asyncio import AsyncServiceBrowser, AsyncServiceInfo
 
 SERVICE = "_printer._tcp.local."
+RE_NAME = "(?P<name>.*?) \([0-9a-f:]{8}\)"
 
 
 class PinCodeScreen(ModalScreen):
@@ -33,11 +38,15 @@ class Printer(ListItem):
 
 
 class PrinterList(ListView):
-    BINDINGS = [("r", "reload", "Reload")]
-
+    BINDINGS = [
+        ("r", "reload", "Reload"),
+        ("a", "add", "Add"),
+        ("r", "remove", "Remove"),
+    ]
+    idx = 0
     browser: AsyncServiceBrowser | None = None
 
-    class New(Message):
+    class NewPrinter(Message):
         """New printer found."""
 
         __slots__ = ["printer_name", "server"]
@@ -51,18 +60,46 @@ class PrinterList(ListView):
             yield "printer_name", self.printer_name
             yield "server", self.server
 
+    class RemovedPrinter(NewPrinter):
+        """Printer must be removed."""
+
+    def action_add(self) -> None:
+        self.idx += 1
+        self._last_name = f"foo_{self.idx}"
+        self.add_printer(PrinterList.NewPrinter(self._last_name, "bar"))
+
+    def action_remove(self) -> None:
+        self.remove_printer(PrinterList.RemovedPrinter(self._last_name, "bar"))
+
     def on_mount(self) -> None:
         self.browse_services()
 
-    @on(New)
-    def add_printer(self, event: New) -> None:
-        self.append(Printer(event.printer_name, event.server))
+    @on(NewPrinter)
+    def add_printer(self, event: NewPrinter) -> None:
+        hash = self.hash_name(event.printer_name)
+        self.append(Printer(event.printer_name, event.server, id=hash))
+
+    @on(RemovedPrinter)
+    def remove_printer(self, event: RemovedPrinter) -> None:
+        print(f"REMOVING {event}")
+        hash = self.hash_name(event.printer_name)
+        widget = self.query_one("#" + hash)
+        print(f"REMOVING WIDGET {widget}")
+        widget.remove()
+
+    def hash_name(self, name: str) -> str:
+        return "P" + hashlib.md5(name.encode()).hexdigest()
 
     @on(ListView.Selected)
-    @work
+    @work()
     async def fix_printer_name(self, event: ListView.Selected) -> None:
-        pin_code = await self.app.push_screen_wait(PinCodeScreen())
-        self.notify(f"Pin code: {pin_code}")
+        # pin_code = await self.app.push_screen_wait(PinCodeScreen())
+        # self.notify("Resetting...")
+        # self.reset_name_through_browser(event.item.server, pin_code)
+        self.post_message(
+            self.RemovedPrinter(event.item.printer_name, event.item.server)
+        )
+        # self.notify("Reset done.")
 
     def browse_services(self):
         def on_service_state_change(
@@ -71,14 +108,24 @@ class PrinterList(ListView):
             name: str,
             state_change=ServiceStateChange,
         ) -> None:
-            asyncio.create_task(get_service_info(zeroconf, service_type, name))
+            asyncio.create_task(
+                get_service_info(zeroconf, service_type, name, state_change)
+            )
+            print(f"{name=}, {state_change=}")
 
         async def get_service_info(
-            zeroconf: Zeroconf, service_type: str, name: str
+            zeroconf: Zeroconf,
+            service_type: str,
+            name: str,
+            state_change: ServiceStateChange,
         ) -> None:
             info = AsyncServiceInfo(service_type, name)
             await info.async_request(zeroconf, 3000)
-            self.post_message(self.New(name, info.server))
+            match state_change:
+                case ServiceStateChange.Added:
+                    self.post_message(self.NewPrinter(name, info.server))
+                case ServiceStateChange.Removed:
+                    self.post_message(self.RemovedPrinter(name, info.server))
 
         self.zeroconf = Zeroconf()
         self.browser = AsyncServiceBrowser(
@@ -93,6 +140,25 @@ class PrinterList(ListView):
             self.browser = None
             self.clear()
             self.browse_services()
+
+    def reset_name_through_browser(self, server: str, pin_code: str) -> None:
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        options.accept_insecure_certs = True
+        with webdriver.Firefox(options=options) as driver:
+            driver.get(f"https://{server}/login.html")
+            driver.find_element(By.ID, "i0012A").click()
+            driver.find_element(By.ID, "i2101").send_keys(pin_code)
+            driver.find_element(By.ID, "submitButton").click()
+
+            driver.get(f"https://{server}/m_network_airprint_edit.html")
+            name_element = driver.find_element(By.ID, "i2072")
+            current_name = name_element.get_attribute("value")
+            if match := re.match(RE_NAME, current_name):
+                printer_name = match.group("name")
+                name_element.clear()
+                name_element.send_keys(printer_name)
+                driver.find_element(By.ID, "submitButton").click()
 
 
 class FixCanonNameApp(App[None]):
